4139166b26f6b85c59968924730ff5714168702e
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-03-19 07:39:08 -0700

    Initial

---
+++ grit.sh
@@ -0,0 +1,16 @@
+function grit() {
+    if [ ! -d "$DIRECTORY" ]; then
+        read -p "Not in a git repo, run git init? " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            git init
+        else
+            return
+        fi
+    fi
+    git config user.email "stripedpajamas273@gmail.com"
+    git config user.name "Peter Squicciarini"
+    git config commit.gpgsign true
+    git config user.signingkey "$GITHUB_GPG_KEY" # should have this set somewhere else 
+    echo "Set email, name, and signing key for this repo."
+}
